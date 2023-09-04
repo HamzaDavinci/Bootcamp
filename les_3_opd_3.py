f85d782c4e84dcfa3ecb902d2160a266416ef2f8
Hamzq <143701275+HamzaDavinci@users.noreply.github.com>
2023-09-04 14:58:47 +0200

    Opdrachten

---
+++ les_3_opd_3.py
@@ -0,0 +1,43 @@
+#opdracht 1 --------------------------------------------------------
+
+leerlingen = 21
+
+bedrag = 1115
+
+result = leerlingen * bedrag 
+
+print ("Zoveel geld kost de opleiding in totaal van de klas:")
+print (result)
+
+#opdracht 1b --------------------------------------------------------
+
+print ("OPDRACHT 1B")
+
+
+appels = 3.40
+
+druiven = 2.45
+
+bananen = 1.95
+print ("Zoveel kost het fruit bij elkaar:")
+result2 = appels + druiven + bananen 
+
+print (result2)
+
+print ("Nu doen we dat gedeeld door 100.")
+
+a = 100
+
+result3 = result2 / a
+
+print (result3)
+print ("Nu doen we dat keer 9.")
+
+b = 9
+
+result4 = result3 * 9
+
+print (result4)
+
+
+
---
+++ les_3_opd_4.py
@@ -0,0 +1,15 @@
+#opdracht 4 --------------------------------------------------------
+
+woord1 = input('wat voor weer is het?')
+stad1 = input('In welke stad?')
+naam1 = input('wat is je naam?')
+woord2 = input('waar ben je naar opzoek?')
+woord3 = input('wat is het persoon die je voor je ziet?')
+woord4 = input('wat verkoopt dat persoon?')
+woord5 = input('waar kwam je')
+woord6 = input('wat zaten jullie te doen?')
+
+
+print('Het was een ' + woord1 + ' dag in ' + stad1 + ' . ' + naam1 +  ' liep door de straten, op zoek naar ' + woord2 + '. Plotseling zag ' + naam1 + ' een ' + woord3 + ' man/vrouw die ' + woord4 + ' verkocht. ' + naam1 + ' besloot om het te kopen en liep verder. Toen ' + naam1 + ' bij een ' + woord5 +  ' kwam, zag hij/zij een groep mensen die ' + woord6 + ' . ' + naam1 + ' besloot om mee te doen en had de tijd van zijn/haar leven.')
+
+#opdracht 4b --------------------------------------------------------
